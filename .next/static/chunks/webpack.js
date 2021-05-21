8f55be220abb3eb90ad8638869a8c6760bffee77
Jeff Passetti <jpassetti@gmail.com>
2021-05-20 20:33:31 -0400

    updated heading styles

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -182,7 +182,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "01e950889fdd02505c0f"; }
+/******/ 		__webpack_require__.h = function() { return "76fdfd656f570bbab70f"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
